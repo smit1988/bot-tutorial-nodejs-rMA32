0b8a25ca58ff96adaac6f388d551a85ffd6077cb
smit1988 <smit1988@purdue.edu>
2016-08-09 10:50:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ server = http.createServer(function (req, res) {
 port = Number(process.env.PORT || 5000);
 server.listen(port);
 
+var fs = require('fs');
+fs.writeFile("message.txt", "Hey there!", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
+
 function ping() {
   this.res.writeHead(200);
   this.res.end("Hey, I'm Cool Guy.");
