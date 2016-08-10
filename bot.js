cd1a3b3dd016a4be1dcba81ca63e07a4611c8dd6
smit1988 <smit1988@purdue.edu>
2016-08-10 10:36:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,9 +90,9 @@ function respond() {
     str = "";
     num = 0;
     postMessage("2");
-    /*str = request.text.substring(10, request.text.length);
+    str = request.text.substring(10, request.text.length);
     postMessage("3" + str);
-    num = str.parseInt();
+    /*num = str.parseInt();
     postMessage("4");
     if(isNaN(num)){
       postMessage(str + " is not a number");
