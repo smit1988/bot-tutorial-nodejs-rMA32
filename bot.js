c52c2beac0366f9d86f5f4c83242eee908922f99
smit1988 <smit1988@purdue.edu>
2016-08-19 23:49:24 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -167,6 +167,8 @@ function respond() {
     this.res.writeHead(200);
     str = request.text.substring(13,request.text.length);
     str = str.trim();
+    str = str.replace("my", "your");
+    str = str.replace("me ", "you ");
     postMessage("Hey " + request.name + ", " + str);
     this.res.end();
   }  
