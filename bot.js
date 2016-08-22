5524eaba1e5825b04609b5a0e93b9d053fd43731
smit1988 <smit1988@purdue.edu>
2016-08-22 17:17:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -218,7 +218,8 @@ function respond() {
   else if(request.text && botRegexTime.test(request.text)) {
     this.res.writeHead(200);
     var dasdf = new Date();
-    postMessage(dasdf.getMinutes());
+    postMessage(dasdf);
+    postMessage(dasdf);
     this.res.end();
   }  
   else if(request.text && botRegexExams.test(request.text)) {
